cfb1722d629f83fb3dea5fd89dac94eb6f26dc7b
cliffox1 <cliffoxstable@gmail.com>
2022-12-11 09:41:22 +0000

    update to architecture diagram

--- starling_test/diagram.py
+++ starling_test/diagram.py
@@ -2,12 +2,12 @@ from diagrams import Diagram, Cluster
 from diagrams.custom import Custom
 from diagrams.onprem.container import Docker
 from diagrams.onprem.database import Postgresql
-from diagrams.onprem.analytics import Metabase
+from diagrams.onprem.analytics import Metabase, Dbt
 
 with Diagram("dbt only", show=False, filename="dbt_diagram", direction="LR", curvestyle='curved'):
     with Cluster(""):
         results = [Custom("Tables", "./resources/database-table.jpg")]
-        dbt = Custom("dbt", "./resources/dbt-logo.png")
+        dbt = Dbt("dbt")
         db = Postgresql("PostgreSQL")
         dbt >> db >> results
         Docker("Docker") >> db
@@ -19,7 +19,7 @@ with Diagram("dbt with metabase", show=False, filename="dbt_metabase_diagram", d
         dashboard = Metabase("Metabase")
         # streamlit = Custom("Streamlit", "./resources/streamlit.png")
         # ui = Custom("Dashboard", "./resources/ui.png")
-        dbt = Custom("dbt", "./resources/dbt-logo.png")
+        dbt = Dbt("dbt")
         results = Custom("Tables", "./resources/database-table.jpg")
         # api = Custom("Source", "./resources/api.png")
         docker >> dashboard
